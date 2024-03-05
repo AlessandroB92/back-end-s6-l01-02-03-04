5b6593aa7fc9d60a99d4d261344921d1c463f445
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-03-05 17:34:00 +0100

    login done

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using back_end_s6_l01_02_03_04.Models;
+
+namespace back_end_s6_l01_02_03_04.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            // cercare l'utente con author.Username e verificare che abbia author.Password nel DB
+            string connString = ConfigurationManager.ConnectionStrings["BRTDbContext"].ToString();
+            var conn = new SqlConnection(connString);
+            conn.Open();
+            var command = new SqlCommand("SELECT * FROM Admin WHERE Username = @username AND Password = @password", conn);
+            command.Parameters.AddWithValue("@username", admin.Username);
+            command.Parameters.AddWithValue("@password", admin.Password);
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                FormsAuthentication.SetAuthCookie(reader["AdminID"].ToString(), true);
+                return RedirectToAction("Index", "Post"); // TODO: alla pagina di pannello
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult Prova()
+        {
+            var adminID = HttpContext.User.Identity.Name;
+            ViewBag.AdminID = adminID;
+            return View();
+        }
+
+        [Authorize, HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            // sloggare l'utente
+            FormsAuthentication.SignOut();
+
+            // ridirezionarlo da qualche parte
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
---
+++ Models/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace back_end_s6_l01_02_03_04.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminID { get; set; }
+
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Min 3, max 20 caratteri")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Min 8, max 15 caratteri")]
+        public string Password { get; set; }
+    }
+}
