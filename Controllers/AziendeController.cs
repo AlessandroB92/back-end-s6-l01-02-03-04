c40e904a474c70963c29d9dd94ede1dc2a81e810
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-03-07 03:58:12 +0100

    add company registration

---
+++ Controllers/AziendeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using back_end_s6_l01_02_03_04.Models;
+
+
+namespace back_end_s6_l01_02_03_04.Controllers
+{        // GET: Aziende
+        public class AziendeController : Controller
+        {
+            public ActionResult Index()
+            {
+                if (!HttpContext.User.Identity.IsAuthenticated)
+                {
+                    ViewBag.ErrorMessage = "Devi effettuare il login per registrare un cliente.";
+                    return View("Error");
+                }
+                return View();
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Index(Aziende azienda)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["BRTDbContext"].ToString();
+                var conn = new SqlConnection(connectionString);
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        conn.Open();
+                        var command = new SqlCommand("INSERT INTO Aziende (PartitaIVA, RagioneSociale, Indirizzo, Email) VALUES (@PartitaIVA, @RagioneSociale, @Indirizzo, @Email)", conn);
+                        command.Parameters.AddWithValue("@PartitaIVA", azienda.PartitaIVA);
+                        command.Parameters.AddWithValue("@RagioneSociale", azienda.RagioneSociale);
+                        command.Parameters.AddWithValue("@Indirizzo", azienda.Indirizzo);
+                        command.Parameters.AddWithValue("@Email", azienda.Email);
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception)
+                    {
+                        ViewBag.ErrorMessage = "Errore nella compilazione dei campi.";
+                        return View("Error");
+                    }
+                    finally { conn.Close(); }
+                    return RedirectToAction("Index", "Home");
+                }
+                return View();
+            }
+        }
+}
--- Models/Aziende.cs
+++ Models/Aziende.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace back_end_s6_l01_02_03_04.Models
 {
     public class Aziende
     {
+        [Key]
         public int AziendaID { get; set; }
         public string PartitaIVA { get; set; }
         public string RagioneSociale { get; set; }
